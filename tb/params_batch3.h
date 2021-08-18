8cccc0303a1b0a8f9556a8dd8a70e9a1b62557c7
Syed Asad Alam <alam.asad@gmail.com>
2021-08-18 14:58:50 +0100

    mlp param

---
+++ tb/params_batch3.h
@@ -0,0 +1,6 @@
+#ifndef PARAMS_HPP
+#define PARAMS_HPP
+namespace PARAM{
+  static FixedPointWeights<64,ap_int<2>,1,1> weights = {{{ap_uint<128>("0x4350c0040d400c0034d1d0d1c5001010", 16)}}};
+}
+#endif
